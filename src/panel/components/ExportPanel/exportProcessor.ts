9c54bc344c125cf9673a35a583204bac3a52694c
Denys Malykhin <denys.malykhin@solidopinion.com>
2022-01-17 00:10:19 -0800

    wip

--- src/panel/components/ExportPanel/exportProcessor.ts
+++ src/panel/components/ExportPanel/exportProcessor.ts
@@ -1,6 +1,32 @@
-import { EventListItem } from 'store/eventRecorderSlice'
+import { EventListItem, IEventPayload } from 'store/eventRecorderSlice'
 import { exportOptions } from './constants'
 import { dump } from 'js-yaml'
+
+enum selectorTypes {
+  role = 'role',
+  labelText = 'label-text',
+  placeholder = 'placeholder',
+  text = 'text',
+  className = 'classname',
+  elementId = 'element-id',
+  testId = 'test-id',
+  uniquePath = 'unique-path',
+}
+
+const selectorsFactoryMap: Record<selectorTypes, (v: string) => string> = {
+  [selectorTypes.role]: (v) => `[role="${v}"]`,
+  [selectorTypes.labelText]: (v) => `tag=label >> text="${v}"`,
+  [selectorTypes.placeholder]: (v) => `[placeholder="${v}"]`,
+  [selectorTypes.text]: (v) => `text="${v}"`,
+  [selectorTypes.className]: (v) => `.${v}`,
+  [selectorTypes.elementId]: (v) => `#${v}`,
+  [selectorTypes.testId]: (v) => `data-test-id=${v}`,
+  [selectorTypes.uniquePath]: (v) => v,
+}
+
+const normalizeString = (str: string) =>
+  // eslint-disable-next-line
+  str.replaceAll('\\', '\\\\').replaceAll('\n', '\\n').replaceAll(`'`, `\\'`)
 
 abstract class ExportProcessor {
   abstract type: exportOptions
@@ -17,7 +43,60 @@ class DakkaProcessor extends ExportProcessor {
   }
 }
 
-const processorsEntries = [DakkaProcessor].map((P) => {
+class PlaywrightProcessor extends ExportProcessor {
+  type = exportOptions.playwright
+  fileName = 'playwright.spec.js'
+
+  private methodsMap: Record<string, (it: IEventPayload) => string> = {
+    mouseClick: () => '.click()',
+    keyboard: ({ key }) => `.fill('${normalizeString(key ?? '')}')`,
+    default: () => '',
+  }
+
+  private getWrapper(testName: string, content: string) {
+    return `const { test, expect } = require('@playwright/test')
+    
+test('${testName}', async ({ page }) => {
+  ${content}
+})`
+  }
+
+  private getGoToTestedPage(url: string) {
+    return `await page.goto('${url}')`
+  }
+
+  private serializeRecordedEvents(events: IEventPayload[]) {
+    return events.reduce((acc, it) => {
+      if (it.type === '_redirect') {
+        acc += '\n  await page.waitForNavigation()\n'
+      }
+      if (it.selectedSelector) {
+        const selector = selectorsFactoryMap[
+          it.selectedSelector.name as selectorTypes
+        ](it.selectedSelector.value)
+        acc += `  await page.locator('${normalizeString(selector)}')${
+          this.methodsMap[it?.type]?.(it) ?? this.methodsMap.default(it)
+        }\n`
+      }
+      return acc
+    }, '')
+  }
+
+  private getContent(events: IEventPayload[]) {
+    const [firstEvent, ...restEvents] = events as IEventPayload[]
+    return `${this.getGoToTestedPage(firstEvent.url ?? '')}
+${this.serializeRecordedEvents(restEvents)}`
+  }
+
+  process(events: EventListItem[]) {
+    const firstRedirect = events[0] as IEventPayload
+    const testName = `Testing ${firstRedirect.url}`
+
+    return this.getWrapper(testName, this.getContent(events as IEventPayload[]))
+  }
+}
+
+const processorsEntries = [PlaywrightProcessor, DakkaProcessor].map((P) => {
   const p = new P()
   return [p.type, p]
 })
@@ -29,7 +108,7 @@ const getProcessor = (type: exportOptions): ExportProcessor =>
 
 export default function process(type: exportOptions, events: EventListItem[]) {
   const p = getProcessor(type)
-
+  console.log(p)
   return {
     text: p.process(events),
     fileName: p.fileName,
