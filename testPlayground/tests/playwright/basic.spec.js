ebf0288a238f1ab9e9b1676873649870ed0e5c30
etunik <helentunik@gmail.com>
2022-02-02 18:15:05 -0800

    add tests

--- testPlayground/tests/playwright/basic.spec.js
+++ testPlayground/tests/playwright/basic.spec.js
@@ -1,4 +1,4 @@
-const { test } = require('@playwright/test')
+const { test, expect } = require('@playwright/test')
 
 test('Testing https://javascript.info/promise-chaining', async ({ page }) => {
   await page.goto('https://javascript.info/promise-chaining')
@@ -7,4 +7,11 @@ test('Testing https://javascript.info/promise-chaining', async ({ page }) => {
   await page.locator('#auth-password').click()
   await page.locator('#auth-password').fill('test')
   await page.locator('button:has-text("Log in")').click()
+  expect(
+    await page.locator(
+      'text="Password incorrect. Or maybe the password is too old, then please update it using password reset."',
+    ),
+  ).toBeTruthy()
+  await page.locator('.close-button:nth-child(1)').click()
+  await page.locator('.sitetoolbar__login').click()
 })
--- testPlayground/tests/puppeteer/basic.spec.js
+++ testPlayground/tests/puppeteer/basic.spec.js
@@ -1,17 +1,40 @@
 const puppeteer = require('puppeteer')
 
-describe('Testing https://vg05y.csb.app/', () => {
-  it('Testing https://vg05y.csb.app/', async () => {
+describe('Testing https://javascript.info/promise-chaining', () => {
+  it('Testing https://javascript.info/promise-chaining', async () => {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
 
     try {
-      await page.goto('https://vg05y.csb.app/')
-      await page.$x("//*[contains(., 'First Name')]").then(async (elements) => {
-        await elements[0].click()
-      })
+      await page.goto('https://javascript.info/promise-chaining')
+      await page.waitForSelector("get('.sitetoolbar__login')")
+      await page.click("get('.sitetoolbar__login')")
+      await page.waitForSelector("get('#auth-email')")
+      await page.type("get('#auth-email')", 'test@gmail.com')
+      await page.waitForSelector("get('#auth-password')")
+      await page.click("get('#auth-password')")
+      await page.waitForSelector("get('#auth-password')")
+      await page.type("get('#auth-password')", 'test')
 
-      expect(await page.$('h1')).toBeDefined()
+      await page.waitForXPath('//button[contains(., "Log in")]')
+      await page
+        .$x('//button[contains(., "Log in")]')
+        .then(async (elements) => {
+          await elements[0].click()
+        })
+
+      await page.waitForXPath(
+        '//p[contains(., "Password incorrect. Or maybe the password is too old, then please update it using password reset.")]',
+      )
+      expect(
+        await page.$x(
+          '//p[contains(., "Password incorrect. Or maybe the password is too old, then please update it using password reset.")]',
+        ),
+      ).toBeDefined()
+      await page.waitForSelector("get('.close-button:nth-child(1)')")
+      await page.click("get('.close-button:nth-child(1)')")
+      await page.waitForSelector("get('.sitetoolbar__login')")
+      await page.click("get('.sitetoolbar__login')")
     } finally {
       await browser.close()
     }
