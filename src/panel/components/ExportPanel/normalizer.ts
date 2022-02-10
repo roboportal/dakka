40f89bcd7d8d9da5e86aa14b21396d9d6b66a8db
Denys Malykhin <denys.malykhin@solidopinion.com>
2022-02-09 19:28:41 -0800

    update

--- src/panel/components/ExportPanel/normalizer.ts
+++ src/panel/components/ExportPanel/normalizer.ts
@@ -1,13 +1,23 @@
+const CHARS_TO_REPLACE_PAIRS = [
+  ['\\', '\\\\'],
+  ['\n', '\\n'],
+  // eslint-disable-next-line
+  ["'", "\\'"],
+  ['"', '\\"'],
+  ['\b', '\\b'],
+  ['\f', '\\f'],
+  ['\r', '\\r'],
+  ['\t', '\\t'],
+  ['\v', '\\v'],
+]
+
 export const normalizeString = (str: string | undefined) => {
   if (!str) {
     return ''
   }
 
-  return (
-    str
-      .replaceAll('\\', '\\\\')
-      .replaceAll('\n', '\\n')
-      // eslint-disable-next-line
-      .replaceAll("'", "\\'")
+  return CHARS_TO_REPLACE_PAIRS.reduce(
+    (acc, [from, to]) => acc.replaceAll(from, to),
+    str,
   )
 }
