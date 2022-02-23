cf15dfecccf6cf038129fa4ecfa9f3ec1ab85783
Denys Malykhin <denys.malykhin@solidopinion.com>
2022-02-22 23:52:18 -0800

    version validation

--- scripts/validate_version.js
+++ scripts/validate_version.js
@@ -1,9 +1,26 @@
 #!/usr/bin/env node
 
-const version = process.argv[2] || ''
+const { version } = require('../package.json')
+const inputVersion = process.argv[2] || ''
 
 const versionRegex = /^\d+\.\d+\.\d+$/
 
-if (!versionRegex.test(version)) {
+if (!versionRegex.test(inputVersion)) {
+  console.error('Invalid version format:', inputVersion)
+  process.exit(1)
+}
+
+const parseVersions = (v) => v.split('.').map((n) => Number(n))
+
+const [patchA, minorA, majorA] = parseVersions(version)
+const [patchB, minorB, majorB] = parseVersions(inputVersion)
+
+const isInputVersionGtExisting =
+  majorB > majorA ||
+  (majorB === majorA && minorB > minorA) ||
+  (majorB === majorA && minorB === minorA && patchB > patchA)
+
+if (!isInputVersionGtExisting) {
+  console.log('Input version is not greater than current')
   process.exit(1)
 }
