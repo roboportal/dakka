9aa533e149e248b9ec10482e303dfd9c3833476a
Denys Malykhin <malykhin@ukr.net>
2022-06-11 21:54:28 -0700

    file select to integration

--- projects/integration/src/index.ts
+++ projects/integration/src/index.ts
@@ -1,8 +1,10 @@
 const checkbox = document.querySelector('#checkbox')
 const button = document.querySelector('#button')
 const input = document.querySelector('#text')
+const file = document.querySelector('#file')
 const textResult = document.querySelector('#text_result')
 const checkboxResult = document.querySelector('#checkbox_result')
+const fileSelectResult = document.querySelector('#file_select_result')
 
 button?.addEventListener('click', () => {
   if (button) {
@@ -22,5 +24,12 @@ checkbox?.addEventListener('click', () => {
 input?.addEventListener('input', (e) => {
   if (textResult) {
     textResult.textContent = (e?.target as any)?.value
+  }
+})
+
+file?.addEventListener('change', (e) => {
+  const [{ name }] = (e?.target as any).files
+  if (fileSelectResult) {
+    fileSelectResult.textContent = name
   }
 })
