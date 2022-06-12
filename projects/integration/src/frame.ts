44b9eb13c86536ee97c0a405ffee7145f3c2742b
Denys Malykhin <malykhin@ukr.net>
2022-06-11 22:57:04 -0700

    file select to integration (#122)
    * file select to integration
    
    * fix
    
    * fix

--- projects/integration/src/frame.ts
+++ projects/integration/src/frame.ts
@@ -1,6 +1,8 @@
 const frameButton = document.querySelector('#button')
 const frameInput = document.querySelector('#text')
+const frameFile = document.querySelector('#file')
 const frameTextResult = document.querySelector('#text_result')
+const frameFileSelectResult = document.querySelector('#file_select_result')
 
 frameButton?.addEventListener('click', () => {
   if (frameButton) {
@@ -11,5 +13,12 @@ frameButton?.addEventListener('click', () => {
 frameInput?.addEventListener('input', (e) => {
   if (frameTextResult) {
     frameTextResult.textContent = (e?.target as any)?.value
+  }
+})
+
+frameFile?.addEventListener('change', (e) => {
+  const [{ name }] = (e?.target as any).files
+  if (frameFileSelectResult) {
+    frameFileSelectResult.textContent = name
   }
 })
